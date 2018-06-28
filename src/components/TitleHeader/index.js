aef57c3ccceb996ca01ec9a7f9b62e2a647dbfa3
phuong.huynh <phuong.huynh@amagumolabs.com>
2018-06-28 10:47:07 +0700

    no message

--- src/components/TitleHeader/index.js
+++ src/components/TitleHeader/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet } from "react-native"
+import { Platform, StatusBar, Text, StyleSheet } from "react-native"
 import { Header, Button, Icon, Left, Right, Body } from "native-base"
 
 export default class TitleHeader extends Component {
   render() {
     return (
-      <Header style={{ backgroundColor: "#92C7A9", paddingTop: 25 }} >
+      <Header style={{ backgroundColor: "#92C7A9", paddingTop: Platform.OS === 'ios' ? 44 : StatusBar.currentHeight }} >
         <Left style={{ flex: 0.2 }}>
           <Button transparent onPress={() => this.props.navigation.openDrawer()} >
             <Icon style={styles.icon} name="menu" type="Entypo" />
